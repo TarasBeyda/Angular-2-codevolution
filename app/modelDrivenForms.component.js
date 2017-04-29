0ee5c5f1e8876bca56d5247cba3db33dcd63e3b2
TarasBeyda <tarasbeyda@gmail.com>
2017-04-30 00:07:15 +0300

    final form builder

--- app/modelDrivenForms.component.js
+++ app/modelDrivenForms.component.js
@@ -5,31 +5,48 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
 var ModelDrivenFormsComponent = (function () {
-    function ModelDrivenFormsComponent() {
+    function ModelDrivenFormsComponent(_formBuilder) {
+        this._formBuilder = _formBuilder;
         this.userForm = new forms_1.FormGroup({
-            name: new forms_1.FormControl('Taras'),
+            name: new forms_1.FormControl('Taras', [forms_1.Validators.required, forms_1.Validators.minLength(4), forms_1.Validators.maxLength(10)]),
             email: new forms_1.FormControl(),
             address: new forms_1.FormGroup({
                 street: new forms_1.FormControl(),
                 city: new forms_1.FormControl(),
-                postalcode: new forms_1.FormControl()
+                postalcode: new forms_1.FormControl(null, forms_1.Validators.pattern('^[1-9][0-9]{4}$'))
             })
         });
     }
     ModelDrivenFormsComponent.prototype.onSubmitModelDrivenForm = function () {
         console.log(this.userForm.value);
     };
+    ModelDrivenFormsComponent.prototype.ngOnInit = function () {
+        this.myFormBuilder = this._formBuilder.group({
+            name: ['Taras', [forms_1.Validators.required, forms_1.Validators.minLength(4), forms_1.Validators.maxLength(10)]],
+            email: [],
+            address: this._formBuilder.group({
+                street: [],
+                city: [],
+                postalcode: [null, [forms_1.Validators.pattern('^[1-9][0-9]{4}$')]]
+            })
+        });
+    };
     return ModelDrivenFormsComponent;
 }());
 ModelDrivenFormsComponent = __decorate([
     core_1.Component({
         selector: 'modelDrivenForms-comp',
-        templateUrl: "app/templates/modelDrivenForms.component.html"
-    })
+        templateUrl: "app/templates/modelDrivenForms.component.html",
+        styles: ["\n                input.ng-invalid {\n                    border-left: 5px solid red;\n                }\n                input.ng-valid {\n                    border-left: 5px solid green;\n                }\n            "]
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
 ], ModelDrivenFormsComponent);
 exports.ModelDrivenFormsComponent = ModelDrivenFormsComponent;
 //# sourceMappingURL=modelDrivenForms.component.js.map
--- app/modelDrivenForms.component.ts
+++ app/modelDrivenForms.component.ts
@@ -1,22 +1,49 @@
-import { Component }                from '@angular/core';
-import { FormGroup, FormControl }   from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { OnInit}  from '@angular/core';
 
 @Component({
     selector: 'modelDrivenForms-comp',
-    templateUrl: `app/templates/modelDrivenForms.component.html`
+    templateUrl: `app/templates/modelDrivenForms.component.html`,
+    styles: [`
+                input.ng-invalid {
+                    border-left: 5px solid red;
+                }
+                input.ng-valid {
+                    border-left: 5px solid green;
+                }
+            `]
 })
 
-export class ModelDrivenFormsComponent {
+export class ModelDrivenFormsComponent implements OnInit {
+
     userForm = new FormGroup({
-        name: new FormControl('Taras'),
+        name: new FormControl('Taras', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
         email: new FormControl(),
         address: new FormGroup({
             street: new FormControl(),
             city: new FormControl(),
-            postalcode: new FormControl()
+            postalcode: new FormControl(null, Validators.pattern('^[1-9][0-9]{4}$'))
         })
     });
     onSubmitModelDrivenForm() {
         console.log(this.userForm.value);
+    }
+
+
+    myFormBuilder: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder){}
+
+    ngOnInit() {
+        this.myFormBuilder = this._formBuilder.group({
+            name: ['Taras', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+            email: [],
+            address: this._formBuilder.group({
+                street: [],
+                city: [],
+                postalcode: [null, [Validators.pattern('^[1-9][0-9]{4}$')]]
+            })
+        })
     }
 }
